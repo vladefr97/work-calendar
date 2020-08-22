5c543ec7aa7da9277700051144c93c421a7aeedb
Vlad Efremov <vladefr97@yandex.ru>
2020-08-22 13:28:52 +0300

    express 5

--- app/serve.js
+++ app/serve.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.port || 80;
+const PORT = process.env.port || 8080;
 app.use(express.static('app'));
 app.get('/', function (req, res) {
     res.sendfile('app/index.html')
